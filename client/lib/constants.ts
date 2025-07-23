10b7026054b88c1e55c1251533fcfdee9f87f69b
David Paarup <david@letsreg.no>
2025-07-23 14:23:16 +0200

    Cleared contact email constant

--- client/lib/constants.ts
+++ client/lib/constants.ts
@@ -3,4 +3,4 @@ export const EVENT_FREQUENCY = "Weekly";
 export const ACTIVE_MEMBERS = 150; 
 export const INSTAGRAM_URL = "https://www.instagram.com/urbansketchers/"; 
 export const FACEBOOK_URL = "https://www.facebook.com/urbansketchers/"; 
-export const CONTACT_EMAIL = "hi@davidpaarup.com"; 
+export const CONTACT_EMAIL = ""; 
